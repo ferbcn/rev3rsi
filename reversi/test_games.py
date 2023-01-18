f8256915a4fc9cb3a5072be1b6c3fd59d7df9bf1
garcifer <garcifer@students.zhaw.ch>
2023-01-18 16:31:44 +0100

    refactoring test boards classes

--- reversi/test_games.py
+++ reversi/test_games.py
@@ -1,49 +1,41 @@
 # Defines special boards for testing game functionality
+class TestGame:
+    def __init__(self, player1, player2, difficulty):
+        self.player1 = player1
+        self.player2 = player2
+        self.difficulty = difficulty
+        self.current_player = 1
 
-class TestGameP1LastMoveToDraw():
-    def __init__(self):
+
+class TestGameP1LastMoveToDraw(TestGame):
+    def __init__(self, player1, player2, difficulty):
+        super().__init__(player1, player2, difficulty)
+
         self.board = [[2, 2, 2, 0, 0, 1, 1, 1] for x in range(8)]
         self.board[0][5] = 0
         self.board[0][6] = 2
 
-        self.player1 = "human"
-        self.player2 = "machine"
-        self.machine_type = "greedy"
-        self.human_player = 1
 
-
-class TestGameP1LastMoveToWin():
+class TestGameP1LastMoveToWin:
     def __init__(self):
+        super().__init__(player1, player2, difficulty)
         self.board = [[2, 2, 2, 0, 0, 1, 1, 1] for x in range(8)]
         self.board[0][5] = 2
-
-        self.player1 = "human"
-        self.player2 = "machine"
-        self.machine_type = "greedy"
-        self.human_player = 1
 
 
 class TestGameP1LastMoveToLose():
     def __init__(self):
+        super().__init__(player1, player2, difficulty)
         self.board = [[2, 2, 2, 0, 0, 0, 1, 1] for x in range(8)]
         self.board[0][5] = 2
-
-        self.player1 = "human"
-        self.player2 = "machine"
-        self.machine_type = "greedy"
-        self.human_player = 1
 
 
 class TestGameP1MoveP2LastMoveToLose():
     def __init__(self):
+        super().__init__(player1, player2, difficulty)
         self.board = [[2, 2, 2, 0, 0, 0, 1, 1] for x in range(8)]
         self.board[0][5] = 2
         self.board[7][3] = 1
-
-        self.player1 = "human"
-        self.player2 = "machine"
-        self.machine_type = "greedy"
-        self.human_player = 1
 
 
 class TestGameP1MoveP2LastMoveToWin():
@@ -51,8 +43,3 @@ class TestGameP1MoveP2LastMoveToWin():
         self.board = [[2, 2, 0, 0, 0, 1, 1, 1] for x in range(8)]
         self.board[0][4] = 2
         self.board[7][2] = 1
-
-        self.player1 = "human"
-        self.player2 = "machine"
-        self.machine_type = "greedy"
-        self.human_player = 1
