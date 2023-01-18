837cf7f404cf2465a79e59b147f8b4f4216594bd
garcifer <garcifer@students.zhaw.ch>
2023-01-18 16:33:15 +0100

    refactoring test boards classes

--- reversi/test_games.py
+++ reversi/test_games.py
@@ -16,30 +16,30 @@ class TestGameP1LastMoveToDraw(TestGame):
         self.board[0][6] = 2
 
 
-class TestGameP1LastMoveToWin:
-    def __init__(self):
+class TestGameP1LastMoveToWin(TestGame):
+    def __init__(self, player1, player2, difficulty):
         super().__init__(player1, player2, difficulty)
         self.board = [[2, 2, 2, 0, 0, 1, 1, 1] for x in range(8)]
         self.board[0][5] = 2
 
 
-class TestGameP1LastMoveToLose():
-    def __init__(self):
+class TestGameP1LastMoveToLose(TestGame):
+    def __init__(self, player1, player2, difficulty):
         super().__init__(player1, player2, difficulty)
         self.board = [[2, 2, 2, 0, 0, 0, 1, 1] for x in range(8)]
         self.board[0][5] = 2
 
 
-class TestGameP1MoveP2LastMoveToLose():
-    def __init__(self):
+class TestGameP1MoveP2LastMoveToLose(TestGame):
+    def __init__(self, player1, player2, difficulty):
         super().__init__(player1, player2, difficulty)
         self.board = [[2, 2, 2, 0, 0, 0, 1, 1] for x in range(8)]
         self.board[0][5] = 2
         self.board[7][3] = 1
 
 
-class TestGameP1MoveP2LastMoveToWin():
-    def __init__(self):
+class TestGameP1MoveP2LastMoveToWin(TestGame):
+    def __init__(self, player1, player2, difficulty):
         self.board = [[2, 2, 0, 0, 0, 1, 1, 1] for x in range(8)]
         self.board[0][4] = 2
         self.board[7][2] = 1
