f4dd25c8018d19de825617a73c21616fde98c63b
garcifer <garcifer@students.zhaw.ch>
2023-01-19 17:32:01 +0100

    www endpoint in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '127.0.0.1', 'reversi.fun']
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'rev3rsi.fun', 'rev3rsi.herokuapp.com']
 
 # Application definition
 
