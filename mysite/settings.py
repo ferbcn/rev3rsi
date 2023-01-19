6934d372c3066f5e19d9c8aeb33faa74c32c621d
garcifer <garcifer@students.zhaw.ch>
2023-01-19 17:09:58 +0100

    www endpoint in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,8 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '127.0.0.1']
-
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'reversi.fun']
 
 # Application definition
 
