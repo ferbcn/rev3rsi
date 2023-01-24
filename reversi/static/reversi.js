edc631188de2e781f3ee2a170446fdb5a2d6bbfb
garcifer <garcifer@students.zhaw.ch>
2023-01-24 19:50:37 +0100

    total logic rework and decoupling

--- reversi/static/reversi.js
+++ reversi/static/reversi.js
@@ -1,65 +1,112 @@
 document.body.style.backgroundColor = "black";
-move(-1,-1);
 var game_over = false;
+// begin by querying the board status and updating its elements
+queryBoard();
+
+
+function queryBoard(){
+    console.log("Querying board...");
+    // Open new request to get new posts.
+    const request = new XMLHttpRequest();
+    request.open('GET', '/queryboard');
+    request.onload = () => {
+        const data = JSON.parse(request.responseText);
+        console.log(data);
+        game_over = data["game_over"]
+
+        // update everything
+        updateAll(data);
+
+        check_for_and_make_auto_machine_move(data)
+    };
+
+    // Send request (only if not game over)
+    if (!game_over){
+        request.send();
+    }
+};
+
 
 function move(row, col){
-    //console.log("Row:", row, "Col:", col);
+    console.log("Making move...");
+    console.log("Row:", row, "Col:", col);
 
     // Open new request to get new posts.
     const request = new XMLHttpRequest();
     request.open('POST', '/move');
     request.onload = () => {
-      //const data = request.responseText;
-      const data = JSON.parse(request.responseText);
-      console.log(data);
+        //const data = request.responseText;
+        const data = JSON.parse(request.responseText);
+        console.log(data);
+        game_over = data["game_over"]
 
-      update_board(data["board"], data["possible_moves"]);
-      // check for player1 being AI move
-      if (data["ai_move"] == true){
-        document.body.style.backgroundColor = "red";
-      }
-      // write message to comm field
-      //var player_tag = 'Player #' + data["player"];
-      var player_tag = 'Human Player';
-      document.getElementById('message_box').innerHTML = data["message"]["message"];
-      var color = data["message"]["color"];
-      document.getElementById('message_box').style = "color: "+color;
+        // update everything
+        updateAll(data);
 
-      document.getElementById('player_box').innerHTML = player_tag;
-      if (data["player"] == 1)
-        document.getElementById('player_box').style = "color: green";
-      else
-        document.getElementById('player_box').style = "color: blue";
+        check_for_and_make_auto_machine_move(data)
+    };
 
-      score_p1 = data["scores"][0];
-      score_p2 = data["scores"][1];
-      scores = "Scores: " + score_p1 + " / " + score_p2;
-      document.getElementById('score_box').innerHTML = scores;
+    // Add row/col to request data.
+    const data = new FormData();
+    data.append('row', row);
+    data.append('col', col);
 
-      if (data["game_over"]){
+    // Send request (only if not game over)
+    if (!game_over){
+        request.send(data);
+    }
+
+    // window.location.reload(true);
+};
+
+
+function check_for_and_make_auto_machine_move(data){
+    if (data["machine_role"] == data["next_player"]){
+        // dummy move make --> machine move (includes delay for better visualization)
+        setTimeout(function() {
+            move(-1, -1)}, 1000);
+    }
+}
+
+
+function updateAll(data){
+    // update board
+    update_board(data["board"], data["possible_moves"]);
+
+    // update scores
+    updateScores(data["scores"])
+
+    // update message
+    updateMessage(data["message"], data["game_over"])
+}
+
+
+function updateScores(scores){
+    score_p1 = scores[0];
+    score_p2 = scores[1];
+    scores = "Scores: " + score_p1 + " / " + score_p2;
+    document.getElementById('score_box').innerHTML = scores;
+}
+
+
+function updateMessage(message, game_over){
+    // write message to comm field
+    document.getElementById('message_box').innerHTML = message["message"];
+    var color = message["color"];
+    document.getElementById('message_box').style = "color: " + color;
+
+    if (game_over){
         if (score_p1 > score_p2)
-            document.getElementById('gameovertext').innerHTML = "YOU WIN!";
+            document.getElementById('gameovertext').innerHTML = "P1 WINS!";
         else if (score_p1 < score_p2)
-            document.getElementById('gameovertext').innerHTML = "YOU LOSE!"
+            document.getElementById('gameovertext').innerHTML = "P2 WINS!";
         else
-          document.getElementById('gameovertext').innerHTML = "DRAW!"
+            document.getElementById('gameovertext').innerHTML = "DRAW!";
         document.getElementById('gameoverbox').classList.add('gameover-box');
         game_over = true;
       }
+}
 
-};
-
-  // Add start and end points to request data.
-  const data = new FormData();
-  data.append('row', row);
-  data.append('col', col);
-
-  // Send request (only if not game over)
-  if (!game_over){
-    request.send(data);
-  }
-  //window.location.reload(true);
-};
 
 function update_board(board, possible_moves){
     for (var r=0; r<8; r++){
@@ -87,8 +134,6 @@ function update_board(board, possible_moves){
               }
             }
           }
-
-
       }
     }
 }
