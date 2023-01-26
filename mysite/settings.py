bdb587b51e1485eafe81532e16058f29b9df22ab
garcifer <garcifer@students.zhaw.ch>
2023-01-26 11:04:40 +0100

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '127.0.0.1', 'rev3rsi.fun', 'rev3rsi.herokuapp.com']
+ALLOWED_HOSTS = ['*', '127.0.0.1']
 
 # Application definition
 
