8082291dad8fe7b348ff539088bda30bdded107f
garcifer <garcifer@students.zhaw.ch>
2023-03-08 22:10:26 +0100

    deploy fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,7 +143,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379), ]
+            "hosts": [("172.17.0.4", 6379), ]
         },
     },
 }
