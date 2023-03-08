5ca1d17dcaa552b1cf6f328175f90f1a98166089
garcifer <garcifer@students.zhaw.ch>
2023-03-08 22:08:50 +0100

    refactor fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,7 +143,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("192.168.0.2", 6379)],
+            "hosts": [("127.0.0.1", 6379), ]
         },
     },
 }
