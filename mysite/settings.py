5dd4be1036b05403f4e664404b594e1cb01653b5
garcifer <garcifer@students.zhaw.ch>
2023-03-09 08:49:36 +0100

    deploy fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -7,6 +7,7 @@ PRODUCTION SETTINGS !!!
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -139,11 +140,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # Channels
 ASGI_APPLICATION = "mysite.asgi.application"
 
+REDIS_HOST = os.environ.get("REDIS_HOST")
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379), ]
+            "hosts": [(REDIS_HOST, 6379), ]
         },
     },
 }
