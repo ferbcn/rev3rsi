dd3aced140154eea54627edba59b492fd8775e9e
garcifer <garcifer@students.zhaw.ch>
2023-03-10 14:30:32 +0100

    new postgres db

--- manage.py
+++ manage.py
@@ -13,17 +13,5 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    # custom run command for IDE development
-    if sys.argv[1] == "runasgi":
-        uvicorn.run(
-            'mysite.asgi:application',
-            host='0.0.0.0',
-            port=8000,
-            reload=True,
-            lifespan="off",
-            # ssl_keyfile='cert/key.pem',
-            # ssl_certfile='cert/cert.pem',
-        )
-    else:
-        execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
 
