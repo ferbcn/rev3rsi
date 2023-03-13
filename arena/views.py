95d624ecc6cf6abe6d90cb88828c892d97461a80
garcifer <garcifer@students.zhaw.ch>
2023-03-13 22:56:11 +0100

    clean-up for deploy

--- arena/views.py
+++ arena/views.py
@@ -5,11 +5,6 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 
-
-#import redis
-#REDIS_HOST = os.environ.get("REDIS_HOST")
-#conn = redis.Redis(host=REDIS_HOST)
-
 game_levels = [('', 'easy'), ('', 'hard'), ('', 'harder'), ('disabled', 'hardest')]
 
 
@@ -19,14 +14,7 @@ def arenaindex(request):
     if request.user.is_authenticated:
         username = request.user.username
 
-        # Read open matches from Redis DB
         nice_open_matches = []
-        """
-        for match, host in open_matches.items():
-            match_name = match.decode("utf-8")
-            match_host = host.decode("utf-8")
-            nice_open_matches.append((match_name, match_host))
-        """
         print("Open matches: ", nice_open_matches)
 
         return render(request, "arena/arena.html", {"open_matches": nice_open_matches, "username": username,
