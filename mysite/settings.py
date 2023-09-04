24d81b1a0453a6a9b376199bf4bcd969bec90bcd
garcifer <garcifer@students.zhaw.ch>
2023-09-04 12:05:58 +0200

    sync to asyn

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['164.92.160.240', 'www.rev3rsi.fun', 'rev3rsi.fun']
 
