fc7fc076a976907afd2fb339fb1d3017c190a1bd
garcifer <garcifer@students.zhaw.ch>
2023-09-21 10:34:31 +0200

    data layer improvements

--- reversi/data_layer.py
+++ reversi/data_layer.py
@@ -3,71 +3,98 @@
 #####################
 
 from .models import GameDB, GameState
+from django.core.exceptions import ObjectDoesNotExist  # Import the necessary exception
 
 
-def save_gamestate_db(board, game_id, prev_state_id):
-    flat_board = [item for row in board for item in row]
-    board_string = ""
-    for num in flat_board:
-        board_string += str(num)
+def save_gamestate_db(board, game_id, prev_id):
+    # Flatten the board and convert to string
+    board_string = ''.join(str(item) for row in board for item in row)
     # print(board_string)
-    gameDB_object = GameDB.objects.get(pk=game_id)
-    game_state_entry = GameState(game_id=gameDB_object, board=board_string) #, prev_state=prev_state_id)
+    # Create and save the new GameState object
+    try:
+        # Fetch the GameDB object
+        game_object = GameDB.objects.get(pk=game_id)
+    except GameDB.DoesNotExist:
+        # Consider logging the error here if needed
+        print(f"No GameDB object found with id: {game_id}")
+        return False  # Indicate that the function did not execute successfully
+
+    game_state_entry = GameState(game_id=game_object, board=board_string) #, prev_state=prev_state_id)
     game_state_entry.save()
-
-
-def save_game_db(game_id, score_p1, score_p2, next_player, end):
-    gameDB_object = GameDB.objects.get(pk=game_id)
-    gameDB_object.game_over = end
-    gameDB_object.score_p1 = score_p1
-    gameDB_object.score_p2 = score_p2
-    gameDB_object.next_player = next_player
-    gameDB_object.save()
     return True
 
 
+def save_game_db(game_id, score_p1, score_p2, next_player, end):
+    try:
+        game_object = GameDB.objects.get(pk=game_id)
+    except ObjectDoesNotExist:
+        # You may want to log this exception, depending on your use case
+        return False  # Return False or appropriate response when the object doesn't exist
+
+    # Update the fields
+    game_object.game_over = end
+    game_object.score_p1 = score_p1
+    game_object.score_p2 = score_p2
+    game_object.next_player = next_player
+
+    game_object.save()  # Save the changes to the database
+
+    return True  # Return True if the function executed successfully
+
+
 def remove_game_db(game_id, user):
-    gameDB_object = GameDB.objects.get(pk=game_id)
-    all_game_states = GameState.objects.all().filter(game_id=gameDB_object)
-    if gameDB_object.user == user:
-        gameDB_object.delete()
+    try:
+        # Directly get the GameDB object
+        game_object = GameDB.objects.get(pk=game_id)
+
+        # Check if the user is the owner of the game
+        if game_object.user != user:
+            return False
+
+        # Fetch all game states associated with the game
+        all_game_states = GameState.objects.filter(game_id=game_object)
+
+        # Delete the game states and the game
         all_game_states.delete()
+        game_object.delete()
+
         return True
-    else:
+    except GameDB.DoesNotExist:
         return False
 
 
 def load_gamestate_db(game_id, user, prev=False):
-    gameDB_object = GameDB.objects.get(pk=game_id)
-    # Only allow players or the owner of the game
-    players = [gameDB_object.player1, gameDB_object.player2]
-    if not user.username in players:
-        if not gameDB_object.user == user:
-            return None
+    # Fetch the GameDB object directly, avoiding unnecessary logic
+    try:
+        game_object = GameDB.objects.get(pk=game_id)
+    except GameDB.DoesNotExist:
+        return None
 
-    # Get Game DB object
-    game_state = GameState.objects.all().filter(game_id=gameDB_object).last()
+    # Check if the user is the owner or one of the players
+    if user.username not in [game_object.player1, game_object.player2] and game_object.user != user:
+        return None
 
+    # Fetch the game state more efficiently
     if prev:
-        game_state = GameState.objects.all().filter(game_id=gameDB_object, prev_state=game_state.prev_state)
+        # Only filter once
+        game_state = GameState.objects.filter(game_id=game_object, prev_state=game_object.prev_state).last()
+    else:
+        game_state = GameState.objects.filter(game_id=game_object).last()
 
+    # Guard against game_state being None
+    if not game_state:
+        return None
 
-    # deserialize board
+    # Deserialize the board using list comprehension
     board_string = game_state.board
-    # print(board_string)
-    game_board = [[0, 0, 0, 0, 0, 0, 0, 0] for x in range(8)]
-    cell = 0
-    for r in range(8):
-        for c in range(8):
-            game_board[r][c] = int(board_string[cell])
-            cell += 1
+    game_board = [[int(board_string[8 * r + c]) for c in range(8)] for r in range(8)]
 
+    # Print the log message (consider changing this to a logger if you are using it in a larger application)
     print("Game Board loaded!", game_state)
-    player1 = gameDB_object.player1
-    player2 = gameDB_object.player2
-    next_player = gameDB_object.next_player
 
-    return game_board, player1, player2, next_player, game_state.id
+    # Return the necessary values directly, avoids redundant assignments
+    return game_board, game_object.player1, game_object.player2, game_object.next_player, game_state.id
+
 
 def get_saved_games_for_user(user):
     return reversed(GameDB.objects.all().filter(user=user).order_by("id"))
